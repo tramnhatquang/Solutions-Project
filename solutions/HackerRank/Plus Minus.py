d5c63af3bbfc1d7405e65984cb9dc5605bd4154c
tramnhatquang <danny.n.tram@gmail.com>
2022-03-23 15:08:57 -0700

    added Plus Minus

---
+++ solutions/HackerRank/Plus Minus.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    length = len(arr)
+    positive_count = 0
+    zero_count = 0
+    for i in arr:
+        if i > 0:
+            positive_count += 1
+        elif i == 0:
+            zero_count += 1
+    # print(positive_count / length)
+    print('{:.6f}'.format(positive_count / length), end='\n')
+    print('{:.6f}'.format((length - positive_count - zero_count) / length),
+          end='\n')
+    print('{:.6f}'.format(zero_count / length), end='\n')
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
