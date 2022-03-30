733d6abeb9c6959beea687f1ac3f1aaf8643393e
tramnhatquang <danny.n.tram@gmail.com>
2022-03-29 17:13:02 -0700

    added new sub-directory

---
+++ Python (HackerRank)/Find a string.py
@@ -0,0 +1,20 @@
+def count_substring(string, sub_string):
+    # Approach 1: Using string slicing
+    # Time: O(n)
+    # Space: O(1)
+    # length_sub = len(sub_string)
+    # count = 0
+    # for i in range(0, len(string)-length_sub+1):
+    #     if string[i:i+length_sub] == sub_string:
+    #         count += 1
+    # return count
+
+    # Approach 2: Using startswith()
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
