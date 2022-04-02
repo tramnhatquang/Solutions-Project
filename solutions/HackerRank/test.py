6a38d0ae894c5448911a08b54c683087fb8a97b1
tramnhatquang <danny.n.tram@gmail.com>
2022-04-01 22:19:33 -0700

    added new sub-directory

--- solutions/HackerRank/test.py
+++ solutions/HackerRank/test.py
@@ -1,11 +1,3 @@
+
 if __name__ == '__main__':
-    for i in range(1, 16):
-        if i % 3 == 0:
-            print('Fizz')
-        elif i % 3 == 0 and i % 5 == 0:
-            print('FizzBuzz1')
-            print('FizzBuzz2')
-        elif i % 5 == 0:
-            print('Buzz')
-        else:
-            print(i)
+    print(1+1)
