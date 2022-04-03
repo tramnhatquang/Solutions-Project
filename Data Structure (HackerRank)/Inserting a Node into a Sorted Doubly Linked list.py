6e6cbf055e58cefbe9ed997e4f19213ab388b341
tramnhatquang <danny.n.tram@gmail.com>
2022-04-02 21:59:52 -0700

    added new files in Data Structure (HackerRank)

---
+++ Data Structure (HackerRank)/Inserting a Node into a Sorted Doubly Linked list.py
@@ -0,0 +1,116 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+        self.tail = node
+
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+
+#
+# Complete the 'sortedInsert' function below.
+#
+# The function is expected to return an INTEGER_DOUBLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_DOUBLY_LINKED_LIST llist
+#  2. INTEGER data
+#
+
+#
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+#
+#
+
+def sortedInsert(llist, data):
+    # Write your code here
+    # if the llist is empty or it only has one element
+    if not llist or not llist.next:
+        return llist
+
+    new_node = DoublyLinkedListNode(data)
+
+    # if the data needs to be inserted at the beginning
+    if llist.data > data:
+        new_node.next = llist
+        llist.prev = new_node
+        return new_node
+
+    curr = llist
+    while curr.next.data < data:
+        curr = curr.next
+
+    # curr is before the inserting node
+    nxt_node = curr.next
+
+    # update the new node and the previous node
+    curr.next = new_node
+    new_node.prev = curr
+
+    # update the new node and the next node
+    new_node.next = nxt_node
+    nxt_node.prev = new_node
+
+    return llist
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        data = int(input())
+
+        llist1 = sortedInsert(llist.head, data)
+
+        print_doubly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
--- Data Structure (HackerRank)/test file.py
+++ Data Structure (HackerRank)/test file.py
@@ -1,10 +1,3 @@
 if __name__ ==  '__main__':
-    a = 2
-    b = 3
-    c = 5
-    if c > b:
-        x = c - b
-    else:
-        x = c - a
-    print(x)
-    print(x + 2)
+    for i in range(1):
+        print(i)
